c3a91c4be84b90df2afebc1accd18fe3020e398e
AnnabelleCat <pikainka@gmail.com>
2018-07-11 20:36:42 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,28 @@
 #write out your code here
 
 def least_coins(cents)
+  number_of_coins = {}
+  
+  #finding the number of quarters
+  number_of_quarters = cents / 25
+  amount_left = cents % 25
+  
+  #finding the number of dimes
+  number_of_dimes = amount_left / 10
+  amount_left_1 = amount_left % 10
+  
+  #finding the number of nickels
+  number_of_nickels = amount_left_1 / 5
+  number_of_pennies = amount_left_1 % 5
+  
+  number_of_coins = {:quarters => number_of_quarters, :dimes => number_of_dimes, :nickles => number_of_nickels, :pennies => number_of_pennies}
+  
+number_of_coins
+  
+  
 
 #Code your answer here!
 
 end
+
+least_coins(29)
